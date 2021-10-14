f989c329171803c9d563b14d6e9cf22d2bd6c382
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-10-14 11:51:13 +0200

    changed ranking routes

--- src/services/ranking.service.js
+++ src/services/ranking.service.js
@@ -1,11 +1,29 @@
-const { User } = require('../models');
+const { Op } = require('sequelize');
+const { Player } = require('../models');
 
 const getAllUsers = async () => {
-  return User.find({ name: { $ne: 'admin' } }).sort({ succes_rate: -1 });
+  return Player.findAll({
+    where: {
+      name: {
+        [Op.ne]: 'admin',
+      },
+    },
+    order: [
+      ['succes_rate', 'DESC'],
+      ['name', 'ASC'],
+    ],
+  });
 };
 
 const getUserBySuccesRate = async (succesRate) => {
-  return User.find({ name: { $ne: 'admin' }, succes_rate: succesRate });
+  return Player.findAll({
+    where: {
+      succes_rate: succesRate,
+      name: {
+        [Op.ne]: 'admin',
+      },
+    },
+  });
 };
 
 module.exports = {
